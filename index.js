7b960f57e0ae48e7796037613ac18b121898f5fb
DARSHAN V <experimenttechchannel@gmail.com>
2026-01-18 17:30:52 +0530

    Update User-Agent header for requests

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get('/proxy/*', async (req, res) => {
         }
 
         const headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+            'User-Agent': 'Mozilla/5.0 (Linux; Android 10; MI 9 Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/111.0.5563.58 Mobile Safari/537.36'
         };
         if (req.headers.range) headers['Range'] = req.headers.range;
 
